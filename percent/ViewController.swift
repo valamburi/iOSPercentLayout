dc22e42e45e0c520872fd1e15680c7a9b66a28dd
Valamburi M <valamburi@auctionsoftware.com>
2015-11-07 16:30:47 +0530

    Added .gitignore

--- percent/ViewController.swift
+++ percent/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myOptionContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = self.myOptionContainer.frame.size.width
+        let height = self.myOptionContainer.frame.size.height
+        
+        let calcHorizontalMargin = width * 0.08
+        let calcVerticalMargin = height * 0.08
+        
+        self.myOptionContainer.layoutMargins = UIEdgeInsetsMake(calcVerticalMargin, calcHorizontalMargin, calcVerticalMargin, calcHorizontalMargin)
+        
+        print("Width: \(width)")
+        print("Height: \(height)")
+        print("Horizontal Margin: \(calcHorizontalMargin)")
+        print("Vertical Margin: \(calcVerticalMargin)")
     }
 
     override func didReceiveMemoryWarning() {
